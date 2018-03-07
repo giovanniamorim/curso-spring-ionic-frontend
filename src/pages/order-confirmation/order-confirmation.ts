97a806638f2c9f9232b7425d9a9cfe18834dc496
Giovanni Amorim <giovanni.amorim.ti@gmail.com>
2018-03-06 22:21:56 -0300

    Informando o pedido registrado ao cliente

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -20,6 +20,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codpedido: string;
 
   constructor(
     public navCtrl: NavController, 
@@ -58,11 +59,15 @@ export class OrderConfirmationPage {
     this.navCtrl.setRoot('CartPage');
   }
 
+  home(){
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
   checkout(){
     this.pedidoService.insert(this.pedido)
       .subscribe(response =>{
         this.cartService.createOrCleanCart();
-        console.log(response.headers.get('location'));
+        this.codpedido = this.extrairId(response.headers.get('location'));
 
       },
       error =>{
@@ -70,6 +75,11 @@ export class OrderConfirmationPage {
           this.navCtrl.setRoot('HomePage');
         }
       });
+  }
+
+  private extrairId(location : string) : string{
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
   }
 
 }
