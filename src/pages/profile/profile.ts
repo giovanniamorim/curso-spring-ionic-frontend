89d0356dda1144b7d8ee89309e79dccc1d9639fd
Giovanni Amorim <giovanni.amorim.ti@gmail.com>
2018-03-07 09:16:17 -0300

    Fazendo o upload da foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -26,6 +26,10 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData(){
     let localUser =  this.storage.getLocalUser();
     if(localUser && localUser.email){
       this.clienteService.findByEmail(localUser.email)
@@ -43,7 +47,6 @@ export class ProfilePage {
       this.navCtrl.setRoot('HomePage');
     }
   }
-
 
   getImageIfExists(){
     this.clienteService.getImageFromBucket(this.cliente.id)
@@ -72,5 +75,20 @@ export class ProfilePage {
     }, (err) => {
      // Handle error
     });
+  }
+
+  sendPicture(){
+    this.clienteService.uploadPicture(this.picture)
+      .subscribe(response =>{
+        this.picture = null;
+        this.loadData();
+      },
+      error=>{
+
+      });
+  }
+
+  cancel(){
+    this.picture = null;
   }
 }
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -4,11 +4,15 @@ import { ClienteDTO } from "../../models/cliente.dto";
 import { Observable } from "rxjs/Rx";
 import { API_CONFIG } from "../../config/api.config";
 import { StorageService } from "../storage.service";
+import { ImageUtilService } from "../image-util.service";
 
 @Injectable()
 export class ClienteService {
 
-    constructor(public http: HttpClient, public storage: StorageService){
+    constructor(
+        public http: HttpClient, 
+        public storage: StorageService,
+        public imageUtilService: ImageUtilService){
 
     }
 
@@ -39,6 +43,21 @@ export class ClienteService {
                 responseType: 'text'
             }
         );
+    }
+
+    uploadPicture(picture){
+        let pictureBlob = this.imageUtilService.dataUriToBlob(picture);
+        let formData : FormData = new FormData();
+        formData.set('file', pictureBlob, 'file.png');
+        return this.http.post(
+            `${API_CONFIG.baseUrl}/clientes/picture`,
+            formData,
+            {
+                observe: 'response',
+                responseType: 'text'
+            }
+        );
+
     }
 
 }
