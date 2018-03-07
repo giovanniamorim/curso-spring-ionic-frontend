ffaad6298285b92c0eb8928b2f2a9c07d4aa5c06
Giovanni Amorim <giovanni.amorim.ti@gmail.com>
2018-03-07 17:18:10 -0300

    Testando o App no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://g-spring-ionic.herokuapp.com/",
     bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/gio-spring-ionic"    
 
 }
